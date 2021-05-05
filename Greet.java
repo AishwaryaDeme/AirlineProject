67d2a09cec874b43c1006e327fbd9112e5168bd9
AishwaryaDeme <aishwaryademe@gmail.com>
2021-05-05 13:44:38 +0530

    This code with changes in class

---
+++ Greet.java
@@ -0,0 +1,14 @@
+
+public class Greet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String msg="Welcome";
+		System.out.println(msg+" to Java");
+		
+		System.out.println("Welcome to Maven Based Core Java Project");
+		System.out.println("Greet Test is calling the class Greet");
+		
+	}
+
+}
