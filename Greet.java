ebf7b92f296c53ef229f12aabcdfb631cabf8d31
Aishwarya <50690430+AishwaryaDeme@users.noreply.github.com>
2021-05-05 13:04:35 +0530

    Add files via upload
    1 line is added

--- Greet.java
+++ Greet.java
@@ -4,6 +4,7 @@ public class Greet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Maven Based Core Java Project");
+		System.out.println("Greet Test is calling the class Greet");
 	}
 
 }
